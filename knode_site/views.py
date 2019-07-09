18c44678191abc3003489d8ac31489e8edcdf482
Graham Droge <grahamdroge@Grahams-MacBook-Pro.local>
2019-07-08 22:26:44 -0500

    Added icons, home.html, and bootstrap app

--- knode_site/views.py
+++ knode_site/views.py
@@ -5,22 +5,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 
-# # Create your views here.
-# def login_user(request):
-# 	username = request.POST['username']
-# 	password = request.POST['password']
-# 	user = authenticate(request, username=username, password=password)
-# 	if user is not None:
-# 		login(request, user)
-# 		return redirect("home")
-# 	else:
-# 		return HttpResponse("Error logging in")
-
-# # Log out user
-# def logout_user(request):
-# 	logout(request)
-# 	return redirect("signin")
-
 # Sign in page
 def signin(request):
 	return render(request, "registration/login.html")
@@ -32,5 +16,5 @@ def signup(request):
 @login_required
 # Home screen
 def home(request):
-	username = request.user.username
-	return HttpResponse("This is the home page for {}".format(username))
+
+	return render(request, "knode_site/home.html")
